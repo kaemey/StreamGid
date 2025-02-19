e9610bc8030ecf1a5199da245a1bdb73cf7ded0f
kaemey <krosto97@yandex.ru>
2025-02-19 13:42:28 +0300

    +++

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required|numeric|digits:10'],
+            'phone' => ['required|numeric'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
